f9c24915b757d05ab982155d48b0f5ae8e64f28a
Flavio Martil <flavio.martil@om30tech.com.br>
2024-05-05 14:23:31 -0300

    Update opcodes

--- src/LogViewerService.php
+++ src/LogViewerService.php
@@ -327,7 +327,7 @@ class LogViewerService
         }
 
         if (class_exists(InstalledVersions::class)) {
-            return InstalledVersions::getPrettyVersion('opcodesio/log-viewer') ?? 'dev-main';
+            return InstalledVersions::getPrettyVersion('flaviowmartil/log-viewer') ?? 'dev-main';
         } else {
             $composerJson = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
 
